7d5cdaadf737bc655cc27234e83da4e18baa7a8e
Dario Protulipac <protulipac@gmail.com>
2019-11-17 23:15:08 +0100

    Inital commit

---
+++ src/main/java/hr/dario/protulipac/photoapp/PhotoappApplication.java
@@ -0,0 +1,13 @@
+package hr.dario.protulipac.photoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/dario/protulipac/photoapp/PhotoappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.dario.protulipac.photoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
