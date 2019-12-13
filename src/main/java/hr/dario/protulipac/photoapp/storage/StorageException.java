41d8cf0fd8490c84dd0af5fc26d4031556c330d7
Dario Protulipac <protulipac@gmail.com>
2019-12-13 14:38:08 +0100

    Uplad picture 1. att

---
+++ src/main/java/hr/dario/protulipac/photoapp/storage/StorageException.java
@@ -0,0 +1,12 @@
+package hr.dario.protulipac.photoapp.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/hr/dario/protulipac/photoapp/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package hr.dario.protulipac.photoapp.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
