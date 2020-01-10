2078149ceafcaf7de2f21e12a197891c522fb546
Dario Protulipac <protulipac@gmail.com>
2020-01-10 20:52:09 +0100

    Some cleanup

--- src/main/java/hr/dario/protulipac/photoapp/controller/FileController.java
+++ src/main/java/hr/dario/protulipac/photoapp/controller/FileController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.ArrayList;
--- src/main/java/hr/dario/protulipac/photoapp/controller/HomeController.java
+++ src/main/java/hr/dario/protulipac/photoapp/controller/HomeController.java
@@ -1,17 +1,14 @@
 package hr.dario.protulipac.photoapp.controller;
 //marin je bio ovdje
+
 import hr.dario.protulipac.photoapp.domain.Picture;
 import hr.dario.protulipac.photoapp.repository.PictureRepo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
