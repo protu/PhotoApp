ea65c4133135e4e6b3973b5a567a49866a151f9c
Dario Protulipac <protulipac@gmail.com>
2020-01-13 23:48:44 +0100

    Clean imports

--- src/main/java/hr/dario/protulipac/photoapp/repository/PictureRepo.java
+++ src/main/java/hr/dario/protulipac/photoapp/repository/PictureRepo.java
@@ -2,14 +2,8 @@ package hr.dario.protulipac.photoapp.repository;
 
 import hr.dario.protulipac.photoapp.domain.Picture;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
 public interface PictureRepo extends CrudRepository<Picture, Long> {
